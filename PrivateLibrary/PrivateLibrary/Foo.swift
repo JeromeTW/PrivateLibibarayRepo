b19a4e6d4eefe039eb6db1b8106222ffbb44f0e8
Jerome.Hsieh <jerome.hsieh@bionime.com>
2018-12-25 16:54:09 +0800

    建立 Framework

---
+++ PrivateLibrary/PrivateLibrary/Foo.swift
@@ -0,0 +1,15 @@
+//
+//  Foo.swift
+//  PrivateLibrary
+//
+//  Created by Jerome.Hsieh2 on 2018/12/25.
+//  Copyright © 2018 bionime. All rights reserved.
+//
+
+import Foundation
+
+class Foo {
+  func foo1() {
+    print("Foo~")
+  }
+}
---
+++ PrivateLibrary/PrivateLibrary/PrivateLibrary.h
@@ -0,0 +1,19 @@
+//
+//  PrivateLibrary.h
+//  PrivateLibrary
+//
+//  Created by Jerome.Hsieh2 on 2018/12/25.
+//  Copyright © 2018 bionime. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PrivateLibrary.
+FOUNDATION_EXPORT double PrivateLibraryVersionNumber;
+
+//! Project version string for PrivateLibrary.
+FOUNDATION_EXPORT const unsigned char PrivateLibraryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PrivateLibrary/PublicHeader.h>
+
+
